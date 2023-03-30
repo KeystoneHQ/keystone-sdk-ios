600a22e4a80f4b8e80f71900efd2f4f72a7692f0
LiYanLance <liyan1924@gmail.com>
2023-03-30 14:32:43 +0800

    Refactor psbt arg and return type from string to data

--- Sources/KeystoneSDK/Chain/KeystoneBitcoinSDK.swift
+++ Sources/KeystoneSDK/Chain/KeystoneBitcoinSDK.swift
@@ -10,18 +10,18 @@ import URRegistryFFI
 import URKit
 
 public class KeystoneBitcoinSDK: KeystoneBaseSDK {
-    public func parsePsbt(cborHex: String) throws -> String {
+    public func parsePSBT(cborHex: String) throws -> Data {
         let psbtURString = handle_error(
             get_result: { parse_crypto_psbt($0, cborHex) }
         )
 
         let psbt = try super.parseUR(urString: psbtURString, ofType: PSBT.self, ofError: KeystoneError.parsePSBTError)
-        return psbt.psbt.uppercased()
+        return psbt.psbt.uppercased().hexadecimal
     }
 
-    public func generatePSBT(psbtHex: String) throws -> UREncoder {
+    public func generatePSBT(psbt: Data) throws -> UREncoder {
         let psbt = handle_error(
-            get_result: { generate_crypto_psbt($0, psbtHex)}
+            get_result: { generate_crypto_psbt($0, psbt.hexEncodedString())}
         )
         return try super.urStringToEncoder(urString: psbt, ofError: KeystoneError.generatePSBTError)
     }
