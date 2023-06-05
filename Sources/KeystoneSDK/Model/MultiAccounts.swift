412067d9b339b4ab38bcfb592e8f8113d323e47f
LiYanLance <liyan1924@gmail.com>
2023-06-05 18:28:24 +0800

    Add extra field in account

--- Sources/KeystoneSDK/Model/MultiAccounts.swift
+++ Sources/KeystoneSDK/Model/MultiAccounts.swift
@@ -9,15 +9,6 @@ import Foundation
 
 public struct MultiAccounts : Equatable, Codable {
     public var masterFingerprint: String
-    public var device: String
+    public var device: String?
     public var keys: Array<Account>
-}
-
-public struct Account : Equatable, Codable {
-    public var chain: String
-    public var path: String
-    public var publicKey: String
-    public var name: String
-    public var chainCode: String
-    public var extendedPublicKey: String
 }
